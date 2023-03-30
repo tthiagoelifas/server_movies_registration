d2428a08639960e9a3b05ac0cb6d8a2c8d41301b
Thiago Elifas <tthiagoelifas@gmail.com>
2023-03-30 12:44:53 -0300

    creating user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,15 +1,21 @@
 const AppError = require("../utils/AppError");
 
-class UsersController{
-  create(req,res){
+const sqliteConnection = require('../database/sqlite')
 
+class UsersController{
+  async create(req,res){
     const {name, email, password} = req.body;
-    
-    if(!name){
-      throw new AppError('Nome é obrigatório !')
+
+    const database = await sqliteConnection();
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)",[email])
+    if(checkUserExists){
+      throw new AppError('Esse e=mail já está em uso.')
     }
+
+    await database.run('INSERT INTO users (name, email, password) VALUES (?,?,?)',[name, email, password])
+
+    return res.status(201).json();
    
-    res.status(201).json({name, email, password})
   }
 
 }
