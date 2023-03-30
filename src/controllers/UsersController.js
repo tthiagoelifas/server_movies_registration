0e63ef51bd17c76317b3413b5f2bb60a507a526a
Thiago Elifas <tthiagoelifas@gmail.com>
2023-03-30 14:03:36 -0300

    creating password cyptografy

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,18 +1,24 @@
+const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
-
-const sqliteConnection = require('../database/sqlite')
+const sqliteConnection = require('../database/sqlite');
 
 class UsersController{
   async create(req,res){
     const {name, email, password} = req.body;
 
     const database = await sqliteConnection();
-    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)",[email])
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)",[email]);
+
     if(checkUserExists){
       throw new AppError('Esse e=mail já está em uso.')
     }
 
-    await database.run('INSERT INTO users (name, email, password) VALUES (?,?,?)',[name, email, password])
+    const hashedPassword = await hash(password, 8);
+
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?) ", 
+      [name, email, hashedPassword]
+    );
 
     return res.status(201).json();
    
