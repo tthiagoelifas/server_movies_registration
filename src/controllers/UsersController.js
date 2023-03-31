a1c72576f215e95947b6b631a4c1f719024c30b4
Thiago Elifas <tthiagoelifas@gmail.com>
2023-03-31 15:42:53 -0300

    create update user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,7 @@
 const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 const sqliteConnection = require('../database/sqlite');
+const usersRoutes = require("../routes/users.routes");
 
 class UsersController{
   async create(req,res){
@@ -22,6 +23,37 @@ class UsersController{
 
     return res.status(201).json();
    
+  }
+
+  async update(req,res){
+    const {name, email} = req.body;
+    const {id} = req.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id]);
+
+    if(!user){
+      throw new AppError('Usuário não encontrado.')
+    }
+
+    const userWithUpdatedEmail = await database.get('SELECT * FROM users WHERE email = (?)',[email]);
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+      throw new AppError('Esse email já está em uso.')
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await database.run(`
+    UPDATE users SET
+    name = ?,
+    email = ?,
+    updated_at = ?
+    WHERE id = ?`,
+    [user.name, user.email, new Date(), id]);
+
+    return res.json();
   }
 
 }
