baadd2ca440877c8e55a0efcf4cbeed9c7c71054
Thiago Elifas <tthiagoelifas@gmail.com>
2023-04-04 09:38:22 -0300

    created NotesControles and its routes

---
+++ src/controllers/NotesController.js
@@ -0,0 +1,29 @@
+const knex = require("../database/knex");
+
+class NotesController{
+  async create(req, res){
+    const {title, description, rating, tags} = req.body;
+    const {user_id} = req.params;
+
+    const [note_id] = await knex("notes").insert({
+      title,
+      description,
+      rating,
+      user_id
+    })
+
+    const tagsInsert = tags.map(name => {
+      return {
+        note_id,
+        name,
+        user_id
+      }
+    })
+
+    await knex("tags").insert(tagsInsert);
+    
+    res.json();
+  }
+}
+
+module.exports = NotesController;
